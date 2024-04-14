835d18aee056e362eb075850fe1a7d694eb3eeb9
julianastahelin <julianastahelin@gmail.com>
2024-04-14 01:38:50 -0300

    fix: add key prop to mapped items

--- app/eventos/page.tsx
+++ app/eventos/page.tsx
@@ -26,7 +26,7 @@ export default async function Eventos() {
         <main className='min-h-screen w-full flex flex-col gap-5 items-center justify-center py-20 text-primary-foreground'>
             <h2 className='text-4xl md:text-5xl tracking-[2px] md:tracking-8px font-bold py-5 md:py-10'>Eventos</h2>
             <div className='flex md:flex-row flex-wrap gap-5 items-center justify-center w-full'>
-                {events.map(event => <EventCard event={event} />)}
+                {events.map((event, index) => <EventCard event={event} key={event.title + index} />)}
             </div>
         </main>
     )
