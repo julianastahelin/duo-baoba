7cacaa141e5486b761b5cbfca0149fa4c842c94d
julianastahelin <julianastahelin@gmail.com>
2024-04-16 14:54:16 -0300

    refactor: give workDetails components descriptive names

--- components/workDetails.tsx
+++ components/workDetails.tsx
@@ -12,17 +12,17 @@ export function WorkDetails({
 }) {
     const { displayType } = work
 
-    if (displayType === '1') return <WorkType1 work={work} id={id} />
-    if (displayType === '2') return <WorkType2 work={work} id={id} />
-    if (displayType === '3') return <WorkType3 work={work} id={id} />
-    if (displayType === '4') return <WorkType4 work={work} id={id} />
-    if (displayType === '5') return <WorkType5 work={work} id={id} />
-    if (displayType === '6') return <WorkType6 work={work} id={id} />
-    if (displayType === '7') return <WorkType7 work={work} id={id} />
-    if (displayType === '8') return <WorkType8 work={work} id={id} />
-    if (displayType === '9') return <WorkType9 work={work} id={id} />
-    if (displayType === '10') return <WorkType10 work={work} id={id} />
-    else return <WorkType1 work={work} id={id} />
+    if (displayType === 'VideoOnly') return <VideoOnly work={work} id={id} />
+    if (displayType === 'FullImgTextLeft') return <FullImgTextLeft work={work} id={id} />
+    if (displayType === 'FullImgTextRight') return <FullImgTextRight work={work} id={id} />
+    if (displayType === 'FullImgTextTop') return <FullImgTextTop work={work} id={id} />
+    if (displayType === 'MdImgTextLeft') return <MdImgTextLeft work={work} id={id} />
+    if (displayType === 'MdImgTextRight') return <MdImgTextRight work={work} id={id} />
+    if (displayType === 'MdImgTextTop') return <MdImgTextTop work={work} id={id} />
+    if (displayType === 'BgImgTextLeft') return <BgImgTextLeft work={work} id={id} />
+    if (displayType === 'BgImgTextRight') return <BgImgTextRight work={work} id={id} />
+    if (displayType === 'BgImgTextBottom') return <BgImgTextBottom work={work} id={id} />
+    else return <FullImgTextLeft work={work} id={id} />
 }
 
 interface WorkTypeProps {
@@ -30,7 +30,7 @@ interface WorkTypeProps {
     id: string
 }
 
-function WorkType1({ work, id }: WorkTypeProps) {
+function FullImgTextLeft({ work, id }: WorkTypeProps) {
     return (
         <div className='min-h-fit w-full flex justify-end'>
             <div className='min-h-fit w-full lg:w-11/12 flex flex-col md:flex-row items-center md:justify-center p-4 md:p-0 md:gap-4' id={id}>
@@ -58,7 +58,7 @@ function WorkType1({ work, id }: WorkTypeProps) {
     )
 }
 
-function WorkType2({ work, id }: WorkTypeProps) {
+function FullImgTextRight({ work, id }: WorkTypeProps) {
     return (
         <div className='min-h-fit w-full flex justify-start' id={id}>
             <div className='min-h-fit w-full lg:w-11/12 flex flex-col-reverse md:flex-row items-center justify-center md:justify-end p-4 md:p-0 md:gap-4 bg-inherit md:box-shadow-none'>
@@ -86,141 +86,7 @@ function WorkType2({ work, id }: WorkTypeProps) {
     )
 }
 
-function WorkType3({ work, id }: WorkTypeProps) {
-    return (
-        <div
-            id={id}
-            style={{ backgroundImage: `url(/assets/img/${work.media})` }}
-            className='h-[60vh] md:h-screen w-full bg-cover bg-center md:bg-left relative flex items-end pb-20 md:pb-0 md:items-center justify-end text-tertiary-foreground text-shadow break-words'
-        >
-            <div className='flex flex-col gap-9 w-2/3 md:w-1/2 max-w-2xl mr-10 xl:mr-14 p-5 bg-secondary rounded-lg'>
-                <h3 className='text-2xl md:text-4xl lg:text-8xl font-medium'>{work.title}</h3>
-                <StyledMarkdown markdown={work.content} className='text-sm md:text-lg lg:text-xl font-normal' />
-            </div>
-            {
-                work.imageCredit
-                    ? <p className='absolute bottom-8 right-12 font-medium text-xs md:text-sm'>Foto: {work.imageCredit}</p>
-                    : ''
-            }
-        </div>
-    )
-}
-
-function WorkType4({ work, id }: WorkTypeProps) {
-    return (
-        <div
-            id={id}
-            style={{ backgroundImage: `url(/assets/img/${work.media})` }}
-            className='h-[60vh] md:h-screen w-full bg-cover bg-center relative'
-        >
-            <div className='h-full w-full bg-quinary flex items-end pb-4 justify-start text-tertiary-foreground text-shadow break-words'>
-                <div className='flex flex-col sm:flex-row items-end gap-4 md:gap-9 w-2/3 md:w-full max-w-7xl ml-10 xl:ml-14 p-5'>
-                    <h3 className='text-3xl md:text-4xl lg:text-7xl font-bold md:font-medium'>{work.title}</h3>
-                    <StyledMarkdown markdown={work.content} className='text-sm md:text-lg lg:text-xl font-normal' />
-                </div>
-                {
-                    work.imageCredit
-                        ? <p className='absolute top-8 left-12 font-medium text-xs md:text-sm'>Foto: {work.imageCredit}</p>
-                        : ''
-                }
-            </div>
-        </div>
-    )
-}
-
-function WorkType5({ work, id }: WorkTypeProps) {
-    return (
-        <div className='w-full min-h-[60vh] md:min-h-fit bg-quaternary flex items-center justify-center text-primary-foreground py-10 md:py-16 lg:py-24 shadow-inside box-shadow-large' id={id}>
-            <div className='flex flex-col w-full md:w-5/6 max-w-full items-center justify-center gap-9 lg:gap-14'>
-                <h3 className='text-3xl md:text-5xl lg:text-7xl font-bold md:font-medium text-wrap break-words px-3'>{work.title}</h3>
-                <div className='flex flex-col md:flex-row items-center gap-5'>
-                    <StyledMarkdown className='text-xs sm:text-sm md:text-base lg:text-lg font-normal w-4/5 md:w-2/5 xl:w-3/5' markdown={work.content} />
-                    {work.media?.includes('youtube')
-                        ? <div className='w-full'>
-                            <Video src={work.media} />
-                        </div>
-                        : <div
-                            style={{ backgroundImage: `url(/assets/img/${work.media})` }}
-                            className='h-[30vh] md:h-[60vh] w-full md:w-3/5 bg-cover bg-center relative'
-                        >
-                            {
-                                work.imageCredit
-                                    ? <p className='absolute bottom-4 md:bottom-6 right-6 md:right-10 font-medium text-sm text-tertiary-foreground text-shadow-sm'>Foto: {work.imageCredit}</p>
-                                    : ''
-                            }
-                        </div>
-                    }
-                </div>
-            </div>
-        </div>
-    )
-}
-
-function WorkType6({ work, id }: WorkTypeProps) {
-    return (
-        <div className='w-full min-h-[60vh] md:min-h-fit bg-tertiary flex items-center justify-center text-primary-foreground py-10 md:py-16 lg:py-24 box-shadow-large md:shadow-inside' id={id}>
-            <div className='flex flex-col w-full md:w-4/5 max-w-7xl items-center justify-center gap-9'>
-                <h3 className='text-3xl md:text-5xl lg:text-7xl font-bold md:font-medium text-center text-wrap break-words px-3'>{work.title}</h3>
-                <div className='flex flex-col md:flex-row items-center justify-center gap-5 md:gap-7'>
-                    {work.media?.includes('youtube')
-                        ? <div className='w-full sm:w-3/4 lg:w-4/5'>
-                            <Video src={work.media} />
-                        </div>
-                        : <div
-                            style={{ backgroundImage: `url(/assets/img/${work.media})` }}
-                            className='h-[40vh] md:h-[60vh] w-full sm:w-3/4 lg:w-4/5 bg-cover bg-center relative'
-                        >
-                            {
-                                work.imageCredit
-                                    ? <p className='absolute bottom-4 md:bottom-6 right-6 md:right-10 font-medium text-sm text-tertiary-foreground text-shadow-sm'>Foto: {work.imageCredit}</p>
-                                    : ''
-                            }
-                        </div>
-                    }
-                    <StyledMarkdown className='text-xs sm:text-sm md:text-base lg:text-lg font-normal w-4/5 md:w-2/5 xl:w-3/5' markdown={work.content} />
-                </div>
-            </div>
-        </div>
-    )
-}
-
-function WorkType7({ work, id }: WorkTypeProps) {
-    return (
-        <div className='w-full min-h-[60vh] md:min-h-fit flex items-center justify-center text-primary-foreground py-10 md:py-16 lg:py-24' id={id}>
-            <div className='flex flex-col w-full md:w-4/5 max-w-7xl items-center justify-center gap-9'>
-                <h3 className='text-3xl md:text-5xl lg:text-7xl font-bold md:font-medium text-wrap break-words px-3'>{work.title}</h3>
-                <div className='flex flex-col items-center gap-5 md:gap-7'>
-                    <StyledMarkdown className='text-xs sm:text-sm font-normal w-3/4 text-center' markdown={work.content} />
-                    {work.media?.includes('youtube')
-                        ? <div className='w-full sm:w-3/4 md:w-4/5'>
-                            <Video src={work.media} />
-                        </div>
-                        : <div
-                            style={{ backgroundImage: `url(/assets/img/${work.media})` }}
-                            className='h-[30vh] md:h-[60vh] w-full md:w-3/5 bg-cover bg-center relative'
-                        >
-                            {
-                                work.imageCredit
-                                    ? <p className='absolute bottom-4 md:bottom-6 right-6 md:right-10 font-medium text-sm text-tertiary-foreground text-shadow-sm'>Foto: {work.imageCredit}</p>
-                                    : ''
-                            }
-                        </div>
-                    }
-                </div>
-            </div>
-        </div>
-    )
-}
-
-function WorkType8({ work, id }: WorkTypeProps) {
-    return (
-        <div className='w-full py-6 md:py-12'>
-            <Video src={work.media} id={id} />
-        </div>
-    )
-}
-
-function WorkType9({ work, id }: WorkTypeProps) {
+function FullImgTextTop({ work, id }: WorkTypeProps) {
     return (
         <div className='w-full min-h-[60vh] md:min-h-screen flex items-center justify-center text-primary-foreground pt-6 md:pt-12' id={id}>
             <div className='flex flex-col w-full max-w-7xl items-center justify-center gap-4 md:gap-9'>
@@ -248,7 +114,7 @@ function WorkType9({ work, id }: WorkTypeProps) {
     )
 }
 
-function WorkType10({ work, id }: WorkTypeProps) {
+function BgImgTextLeft({ work, id }: WorkTypeProps) {
     return (
         <div
             id={id}
@@ -264,6 +130,140 @@ function WorkType10({ work, id }: WorkTypeProps) {
                     ? <p className='absolute bottom-8 right-12 font-medium text-xs md:text-sm'>Foto: {work.imageCredit}</p>
                     : ''
             }
+        </div>
+    )
+}
+
+function BgImgTextRight({ work, id }: WorkTypeProps) {
+    return (
+        <div
+            id={id}
+            style={{ backgroundImage: `url(/assets/img/${work.media})` }}
+            className='h-[60vh] md:h-screen w-full bg-cover bg-center md:bg-left relative flex items-end pb-20 md:pb-0 md:items-center justify-end text-tertiary-foreground text-shadow break-words'
+        >
+            <div className='flex flex-col gap-9 w-2/3 md:w-1/2 max-w-2xl mr-10 xl:mr-14 p-5 bg-secondary rounded-lg'>
+                <h3 className='text-2xl md:text-4xl lg:text-8xl font-medium'>{work.title}</h3>
+                <StyledMarkdown markdown={work.content} className='text-sm md:text-lg lg:text-xl font-normal' />
+            </div>
+            {
+                work.imageCredit
+                    ? <p className='absolute bottom-8 right-12 font-medium text-xs md:text-sm'>Foto: {work.imageCredit}</p>
+                    : ''
+            }
+        </div>
+    )
+}
+
+function BgImgTextBottom({ work, id }: WorkTypeProps) {
+    return (
+        <div
+            id={id}
+            style={{ backgroundImage: `url(/assets/img/${work.media})` }}
+            className='h-[60vh] md:h-screen w-full bg-cover bg-center relative'
+        >
+            <div className='h-full w-full bg-quinary flex items-end pb-4 justify-start text-tertiary-foreground text-shadow break-words'>
+                <div className='flex flex-col sm:flex-row items-end gap-4 md:gap-9 w-2/3 md:w-full max-w-7xl ml-10 xl:ml-14 p-5'>
+                    <h3 className='text-3xl md:text-4xl lg:text-7xl font-bold md:font-medium'>{work.title}</h3>
+                    <StyledMarkdown markdown={work.content} className='text-sm md:text-lg lg:text-xl font-normal' />
+                </div>
+                {
+                    work.imageCredit
+                        ? <p className='absolute top-8 left-12 font-medium text-xs md:text-sm'>Foto: {work.imageCredit}</p>
+                        : ''
+                }
+            </div>
+        </div>
+    )
+}
+
+function MdImgTextLeft({ work, id }: WorkTypeProps) {
+    return (
+        <div className='w-full min-h-[60vh] md:min-h-fit bg-quaternary flex items-center justify-center text-primary-foreground py-10 md:py-16 lg:py-24 shadow-inside box-shadow-large' id={id}>
+            <div className='flex flex-col w-full md:w-5/6 max-w-full items-center justify-center gap-9 lg:gap-14'>
+                <h3 className='text-3xl md:text-5xl lg:text-7xl font-bold md:font-medium text-wrap break-words px-3'>{work.title}</h3>
+                <div className='flex flex-col md:flex-row items-center gap-5'>
+                    <StyledMarkdown className='text-xs sm:text-sm md:text-base lg:text-lg font-normal w-4/5 md:w-2/5 xl:w-3/5' markdown={work.content} />
+                    {work.media?.includes('youtube')
+                        ? <div className='w-full'>
+                            <Video src={work.media} />
+                        </div>
+                        : <div
+                            style={{ backgroundImage: `url(/assets/img/${work.media})` }}
+                            className='h-[30vh] md:h-[60vh] w-full md:w-3/5 bg-cover bg-center relative'
+                        >
+                            {
+                                work.imageCredit
+                                    ? <p className='absolute bottom-4 md:bottom-6 right-6 md:right-10 font-medium text-sm text-tertiary-foreground text-shadow-sm'>Foto: {work.imageCredit}</p>
+                                    : ''
+                            }
+                        </div>
+                    }
+                </div>
+            </div>
+        </div>
+    )
+}
+
+function MdImgTextRight({ work, id }: WorkTypeProps) {
+    return (
+        <div className='w-full min-h-[60vh] md:min-h-fit bg-tertiary flex items-center justify-center text-primary-foreground py-10 md:py-16 lg:py-24 box-shadow-large md:shadow-inside' id={id}>
+            <div className='flex flex-col w-full md:w-4/5 max-w-7xl items-center justify-center gap-9'>
+                <h3 className='text-3xl md:text-5xl lg:text-7xl font-bold md:font-medium text-center text-wrap break-words px-3'>{work.title}</h3>
+                <div className='flex flex-col md:flex-row items-center justify-center gap-5 md:gap-7'>
+                    {work.media?.includes('youtube')
+                        ? <div className='w-full sm:w-3/4 lg:w-4/5'>
+                            <Video src={work.media} />
+                        </div>
+                        : <div
+                            style={{ backgroundImage: `url(/assets/img/${work.media})` }}
+                            className='h-[40vh] md:h-[60vh] w-full sm:w-3/4 lg:w-4/5 bg-cover bg-center relative'
+                        >
+                            {
+                                work.imageCredit
+                                    ? <p className='absolute bottom-4 md:bottom-6 right-6 md:right-10 font-medium text-sm text-tertiary-foreground text-shadow-sm'>Foto: {work.imageCredit}</p>
+                                    : ''
+                            }
+                        </div>
+                    }
+                    <StyledMarkdown className='text-xs sm:text-sm md:text-base lg:text-lg font-normal w-4/5 md:w-2/5 xl:w-3/5' markdown={work.content} />
+                </div>
+            </div>
+        </div>
+    )
+}
+
+function MdImgTextTop({ work, id }: WorkTypeProps) {
+    return (
+        <div className='w-full min-h-[60vh] md:min-h-fit flex items-center justify-center text-primary-foreground py-10 md:py-16 lg:py-24' id={id}>
+            <div className='flex flex-col w-full md:w-4/5 max-w-7xl items-center justify-center gap-9'>
+                <h3 className='text-3xl md:text-5xl lg:text-7xl font-bold md:font-medium text-wrap break-words px-3'>{work.title}</h3>
+                <div className='flex flex-col items-center gap-5 md:gap-7'>
+                    <StyledMarkdown className='text-xs sm:text-sm font-normal w-3/4 text-center' markdown={work.content} />
+                    {work.media?.includes('youtube')
+                        ? <div className='w-full sm:w-3/4 md:w-4/5'>
+                            <Video src={work.media} />
+                        </div>
+                        : <div
+                            style={{ backgroundImage: `url(/assets/img/${work.media})` }}
+                            className='h-[30vh] md:h-[60vh] w-full md:w-3/5 bg-cover bg-center relative'
+                        >
+                            {
+                                work.imageCredit
+                                    ? <p className='absolute bottom-4 md:bottom-6 right-6 md:right-10 font-medium text-sm text-tertiary-foreground text-shadow-sm'>Foto: {work.imageCredit}</p>
+                                    : ''
+                            }
+                        </div>
+                    }
+                </div>
+            </div>
+        </div>
+    )
+}
+
+function VideoOnly({ work, id }: WorkTypeProps) {
+    return (
+        <div className='w-full py-6 md:py-12'>
+            <Video src={work.media} id={id} />
         </div>
     )
 }
