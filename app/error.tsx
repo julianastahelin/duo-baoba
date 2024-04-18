da67f59859939e57c8c9d154c56fbb17ee227184
julianastahelin <julianastahelin@gmail.com>
2024-04-18 16:37:46 -0300

    refactor: reorder error custom pages

--- app/error.tsx
+++ app/error.tsx
@@ -14,12 +14,12 @@ export default function Error({
   }, [error])
 
   return (
-    <div className='w-full h-screen flex flex-col items-center justify-center'>
-      <h2 className='text-primary-foreground text-3xl'>Something went wrong!</h2>
+    <div className='w-full h-screen flex flex-col items-center justify-center gap-4'>
+      <h2 className='text-primary-foreground text-3xl'>Ocorreu um erro!</h2>
       <button className='p-5 rounded-md text-lg font-normal bg-primary-foreground text-tertiary-foreground hover:scale-105 hover:transition-all hover:duration-200 hover:opacity-95'
         onClick={() => reset()}
       >
-        Try again
+        Tentar novamente
       </button>
     </div>
   )
