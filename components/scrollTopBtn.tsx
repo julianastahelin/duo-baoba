7f573cb8b0bee80a63944266dac5b7a2ac7f001c
julianastahelin <julianastahelin@gmail.com>
2024-04-19 12:15:59 -0300

    fix: change button sizes in small and large screens

--- components/scrollTopBtn.tsx
+++ components/scrollTopBtn.tsx
@@ -16,9 +16,9 @@ export function ScrollTopButton() {
     return (
         <button
             onClick={scrollToTop}
-            className='fixed bottom-0 right-0 m-5 md:m-10 p-1 rounded-full bg-primary-foreground hover:scale-105 hover:opacity-90 transition-all shadow-md shadow-shadowColor'
+            className='fixed bottom-0 right-0 m-8 md:m-10 p-1 rounded-full bg-primary-foreground hover:scale-105 hover:opacity-90 transition-all shadow-md shadow-shadowColor'
         >
-            <ArrowUp className='text-secondary-foreground w-5 h-5 lg:w-6 lg:h-6' />
+            <ArrowUp className='text-secondary-foreground w-6 h-6 2xl:w-8 2xl:h-8' />
         </button>
     )
 }
