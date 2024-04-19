0fa1bd470bc46cf1d7b6fdb0d87e517dac7a99e9
julianastahelin <julianastahelin@gmail.com>
2024-04-19 09:22:54 -0300

    refactor: build array and map contact items for rendering

--- components/personalContact.tsx
+++ components/personalContact.tsx
@@ -7,7 +7,7 @@ import { Icon, IconName } from './icons'
 
 export function PersonalContact({ profile }: { profile: Profile }) {
     const { toast } = useToast()
-    
+
     function copyAddress(address: string) {
         navigator.clipboard.writeText(address)
         toast({
@@ -16,30 +16,48 @@ export function PersonalContact({ profile }: { profile: Profile }) {
         })
     }
 
+    const profileInstagramUsername = `@${profile.instagram.split('/')[3]}`
     const profilePhoneNumsOnly = profile.phone.split('').map(char => /^[0-9]*$/.test(char) ? char : '').join('')
-    
+
+    const profileContacts = [
+        {
+            type: 'Instagram',
+            link: profile.instagram,
+            title: profileInstagramUsername
+        },
+        {
+            type: 'Whatsapp',
+            link: `https://wa.me/55${profilePhoneNumsOnly}`,
+            title: profile.phone
+        },
+        {
+            type: 'Email',
+            link: profile.email,
+            title: profile.email
+        }
+    ]
+
     return (
         <div className='text-primary-foreground flex flex-col items-center justify-center gap-6 p-10 break-words'>
             <p className='text-center text-lg md:text-3xl tracking-[2px] md:tracking-[5px]'>Quer contratar {profile.name}? Entre em contato!</p>
             <ul className='flex flex-col gap-2 text-sm sm:text-base w-auto max-w-full'>
-                <li>
-                    <a href={profile.instagram} target='_blank' className='flex flex-wrap gap-1 items-center w-full'>
-                        <Icon name={'Instagram' as IconName} className='w-7 h-7 md:w-8 md:h-8 fill-primary-foreground' />
-                        <span className='underline max-w-full'>@{profile.instagram.split('/')[3]}</span>
-                    </a>
-                </li>
-                <li>
-                    <a href={`https://wa.me/55${profilePhoneNumsOnly}`} target='_blank' className='flex flex-wrap gap-1 items-center w-full'>
-                        <Icon name={'Whatsapp' as IconName} className='w-7 h-7 md:w-8 md:h-8 fill-primary-foreground stroke-1' />
-                        <span className='underline max-w-full'>{profile.phone}</span>
-                    </a>
-                </li>
-                <li>
-                    <button onClick={() => copyAddress(profile.email)} className='flex flex-wrap gap-1 items-center w-full'>
-                        <Icon name={'Email' as IconName} className='w-7 h-7 md:w-8 md:h-8 stroke-1 fill-primary-foreground stroke-tertiary' />
-                        <span className='underline max-w-full'>{profile.email}</span>
-                    </button>
-                </li>
+                {profileContacts.map((contact, index) => {
+                    return (
+                        <li key={contact.type + index}>
+                            {
+                                contact.type === 'Email'
+                                    ? <a onClick={() => copyAddress(profile.email)} className='flex flex-wrap gap-1 items-center w-full hover:opacity-80 transition-all'>
+                                        <Icon name={contact.type as IconName} className='w-7 h-7 md:w-8 md:h-8 fill-primary-foreground stroke-1 stroke-tertiary' />
+                                        <span className='underline max-w-full'>{contact.title}</span>
+                                    </a>
+                                    : <a href={contact.link} target='_blank' className='flex flex-wrap gap-1 items-center w-full hover:opacity-80 transition-all'>
+                                        <Icon name={contact.type as IconName} className='w-7 h-7 md:w-8 md:h-8 fill-primary-foreground' />
+                                        <span className='underline max-w-full'>{contact.title}</span>
+                                    </a>
+                            }
+                        </li>
+                    )
+                })}
             </ul>
         </div>
     )
