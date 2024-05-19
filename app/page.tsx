6ef030012ae81881ba1dd72b5257dd8e608d2c9e
julianastahelin <julianastahelin@gmail.com>
2024-05-19 18:50:53 -0300

    feat: animate all pages

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,8 @@
 import Image from 'next/image'
 
-import { Video } from '@/components/video'
+import { AnimatedDiv, AnimatedFooter, AnimatedH1, AnimatedHeader, AnimatedParagraph, AnimatedSection } from '@/components/animated-ui'
 import { StyledMarkdown } from '@/components/styledMarkdown'
+import { Video } from '@/components/video'
 import { SocialMedias } from '@/components/socialMedias'
 import { getHomeSections, getSocialMedias } from '@/services/notion'
 
@@ -12,9 +13,9 @@ export default async function Home() {
 
   return (
     <>
-      <header className="flex flex-col gap-5 p-8 pt-16 md:p-0 md:items-center md:h-screen md:justify-center md:bg-[url('/assets/img/home-header-img.png')] md:bg-cover md:bg-center text-primary-foreground md:text-secondary-foreground text-shadow-sm md:text-shadow">
+      <AnimatedHeader className="flex flex-col gap-5 p-8 pt-28 md:p-0 md:items-center md:h-screen md:justify-center md:bg-[url('/assets/img/home-header-img.png')] md:bg-cover md:bg-center text-primary-foreground md:text-secondary-foreground text-shadow-sm md:text-shadow">
         <div className='tracking-[5px] md:z-10'>
-          <h1 className='flex flex-col gap-2 items-center'>
+          <AnimatedH1 className='flex flex-col gap-2 items-center'>
             <span className='text-3xl md:text-4xl lg:text-6xl font-light'>
               Duo
             </span>
@@ -22,38 +23,42 @@ export default async function Home() {
             <span className='font-bold text-6xl md:text-7xl lg:text-9xl'>
               Baobá
             </span>
-          </h1>
-          <p className='font-light text-base md:text-xl lg:text-3xl text-center'>· Flauta · Violão ·</p>
+          </AnimatedH1>
+          <AnimatedParagraph className='font-light text-base md:text-xl lg:text-3xl text-center'>· Flauta · Violão ·</AnimatedParagraph>
         </div>
-        <Image src='/assets/img/home-header-img.png' className='w-full min-w-52 md:hidden' width={300} height={500} alt='Foto do Duo Baobá - Mayara e David' title='Foto do Duo Baobá - Mayara e David' />
-      </header>
-      <div className='bg-tertiary py-5 w-full' />
+        <AnimatedDiv delay={0.3} duration={0.7} scale={1.1}>
+          <Image src='/assets/img/home-header-img.png' className='w-full min-w-52 pb-16 md:hidden' width={300} height={500} alt='Foto do Duo Baobá - Mayara e David' title='Foto do Duo Baobá - Mayara e David' />
+          </AnimatedDiv>
+      </AnimatedHeader>
+      <AnimatedDiv className='bg-tertiary py-5 w-full' delay={1.2} />
 
       <main className='flex flex-col items-center justify-center py-16 md:py-32 gap-12 text-center text-primary-foreground'>
         {
-          homeSections.map((section, index) => {
-            return (
-              <section key={section.title + index} className='flex flex-col gap-5 w-5/6 md:w-3/5 items-center'>
-                <h2 className='text-2xl sm:text-5xl tracking-[4px] md:tracking-8px font-bold'>· {section.title} ·</h2>
-                <StyledMarkdown markdown={section.content} className='text-sm sm:text-xl font-light' />
-                {section.media
-                  ? section.media.includes('youtube')
-                    ? <div className='w-full py-2 md:py-6'>
-                      <Video src={`${section.media}?autoplay=1&mute=1`} />
-                    </div>
-                    : <Image src={`/assets/img/${section.media}`} alt={`${section.title}`} height={300} width={500} className='py-12' />
-                  : ''
-                }
-              </section>
-            )
-          })
+          homeSections.map((section, index) => (
+            <AnimatedSection
+              index={index}
+              className='flex flex-col gap-5 w-5/6 md:w-3/5 items-center'
+              key={section.title + index}
+            >
+              <h2 className='text-2xl sm:text-5xl tracking-[4px] md:tracking-8px font-bold'>· {section.title} ·</h2>
+              <StyledMarkdown markdown={section.content} className='text-sm sm:text-xl font-light' />
+              {section.media
+                ? section.media.includes('youtube')
+                  ? <div className='w-full py-2 md:py-6'>
+                    <Video src={`${section.media}?autoplay=1&mute=1`} />
+                  </div>
+                  : <Image src={`/assets/img/${section.media}`} alt={`${section.title}`} height={300} width={500} className='py-12' />
+                : ''
+              }
+            </AnimatedSection>
+          ))
         }
       </main>
 
-      <footer className='flex flex-col items-center p-10 gap-7 text-primary-foreground'>
+      <AnimatedFooter className='flex flex-col items-center p-10 gap-7 text-primary-foreground'>
         <h3 className='text-center text-lg md:text-3xl tracking-[2px] md:tracking-[5px]'>Acompanhe nossas redes sociais!</h3>
         <SocialMedias socialMedias={socialMedias} />
-      </footer>
+      </AnimatedFooter>
     </>
   )
 }
--- app/sobre/[slug]/page.tsx
+++ app/sobre/[slug]/page.tsx
@@ -2,6 +2,7 @@ import Image from 'next/image'
 import Link from 'next/link'
 import { redirect } from 'next/navigation'
 
+import { AnimatedFooter, AnimatedHeader, AnimatedH1, AnimatedParagraph, AnimatedSection, AnimatedDiv } from '@/components/animated-ui'
 import { PersonalContact } from '@/components/personalContact'
 import { StyledMarkdown } from '@/components/styledMarkdown'
 import { WorkDetails } from '@/components/workDetails'
@@ -54,21 +55,21 @@ export default async function About({ params }: PageProps) {
         return (
             <>
                 <div></div>
-                <header
+                <AnimatedHeader
                     style={{ '--image-url': `url(/assets/img/${profile.picture})` } as React.CSSProperties}
                     className='pt-24 md:pt-20 mb-20 w-full md:min-h-screen bg-primary md:bg-[image:var(--image-url)] md:bg-no-repeat md:bg-cover md:bg-right lg:bg-center flex flex-col justify-center md:justify-between items-center lg:items-start gap-10'
                 >
                     <div></div>
                     <div className='flex flex-col gap-4 lg:gap-8 text-primary-foreground md:text-secondary-foreground text-shadow-sm md:text-shadow items-center p-3 text-center  lg:pl-32 xl:pl-48'>
-                        <h1 className='text-4xl tracking-[2px] md:text-6xl lg:text-[80px] font-bold md:tracking-[5px]'>{profile.name}</h1>
-                        <p className='text-base tracking-[1px] md:text-xl lg:text-3xl font-light md:tracking-[4px]'>· {profile.tags.join(' · ')} ·</p>
+                        <AnimatedH1 className='text-4xl tracking-[2px] md:text-6xl lg:text-[80px] font-bold md:tracking-[5px]'>{profile.name}</AnimatedH1>
+                        <AnimatedParagraph className='text-base tracking-[1px] md:text-xl lg:text-3xl font-light md:tracking-[4px]'>· {profile.tags.join(' · ')} ·</AnimatedParagraph>
                     </div>
                     <Image src={`/assets/img/${profile.picture}`} className='w-full min-w-52 md:hidden' width={300} height={500} alt={`Profile picture of ${profile.name}`} />
                     <div className='h-16 w-full bg-tertiary opacity-80 hidden md:flex md:justify-self-end'></div>
-                </header>
+                </AnimatedHeader>
 
                 <main>
-                    <section className='flex flex-col items-center justify-center py-12 gap-10 md:gap-16 text-primary-foreground'>
+                    <AnimatedSection index={1} className='flex flex-col items-center justify-center py-12 gap-10 md:gap-16 text-primary-foreground'>
                         <h2 className='text-3xl md:text-4xl lg:text-6xl font-medium text-wrap justify-self-center tracking-[2px] md:tracking-[5px]'>· Formação ·</h2>
                         <ul className='flex flex-col sm:flex-row gap-5 md:gap-10 items-center w-3/4'>
                             {academicEducation.map((course, index) => {
@@ -97,8 +98,8 @@ export default async function About({ params }: PageProps) {
                             </div>
                             : ''
                         }
-                    </section>
-                    <section className='flex flex-col items-center justify-center'>
+                    </AnimatedSection>
+                    <AnimatedSection index={2} className='flex flex-col items-center justify-center'>
                         <h2 className='text-3xl md:text-4xl lg:text-6xl font-medium text-wrap justify-self-center tracking-[2px] md:tracking-[5px] text-primary-foreground py-6'>· Trabalhos ·</h2>
                         <div className='grid grid-cols-xs sm:grid-cols-sm min-[500px]:grid-cols-xs2 md:grid-cols-md xl:grid-cols-xl gap-2 sm:gap-4 p-3 xl:py-6 items-center justify-center bg-quaternary box-shadow-large w-full'>
                             {works.map((work, index) => {
@@ -110,7 +111,7 @@ export default async function About({ params }: PageProps) {
                                                 ? `url(/assets/img/${stringsToKebabCase([params.slug, work.title])}.png)`
                                                 : `url(/assets/img/${work.media})`
                                         } as React.CSSProperties}
-                                        className='bg-[image:var(--image-url)] bg-cover bg-center aspect-square object-contain w-full flex flex-col justify-end py-2 sm:py-5 px-2 sm:px-6 box-shadow-large'>
+                                        className='bg-[image:var(--image-url)] bg-cover bg-center aspect-square object-contain w-full flex flex-col justify-end py-2 sm:py-5 px-2 sm:px-6 box-shadow-large hover:scale-[1.02] hover:opacity-95 transition-all ease-in-out duration-400'>
                                         <p className='text-tertiary-foreground font-medium text-xs sm:text-xl md:text-3xl lg:text-4xl text-shadow-sm'>{work.title}</p>
                                     </Link>
                                 )
@@ -118,15 +119,17 @@ export default async function About({ params }: PageProps) {
                         </div>
                         {works.map((work, index) => {
                             return (
-                                <WorkDetails work={work} id={`${(index + 1)}-${work.title}`} key={work.title + index} />
+                                <AnimatedDiv className='w-full' y={15} delay={0}>
+                                    <WorkDetails work={work} id={`${(index + 1)}-${work.title}`} key={work.title + index} />
+                                </AnimatedDiv>
                             )
                         })}
-                    </section>
+                    </AnimatedSection>
                 </main>
 
-                <footer className='box-shadow-large'>
+                <AnimatedFooter className='box-shadow-large'>
                     <PersonalContact profile={profile} />
-                </footer>
+                </AnimatedFooter>
             </>
         )
 }
