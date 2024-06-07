17dec82d44596620dd43b5f4c973d80a3e1d8f35
julianastahelin <julianastahelin@gmail.com>
2024-06-07 16:09:23 -0300

    fix: avoid h1 and h2 className override on styledMarkdown

--- components/styledMarkdown.tsx
+++ components/styledMarkdown.tsx
@@ -10,11 +10,11 @@ export function StyledMarkdown({
 }) {
     return (
         <Markdown
-            components={{
-                h1: ({ node, ...props}) => <h1 className='text-xl font-bold' {...props} />,
-                h2: ({ node, ...props}) => <h2 className='text-lg font-semibold' {...props} />
-            }}
             className={className}
+            components={{
+                h1: ({ node, ...props }) => <h1 className='text-xl font-bold' {...props} />,
+                h2: ({ node, ...props }) => <h2 className='text-lg font-semibold' {...props} />
+            }}
         >
             {markdown}
         </Markdown>
