3353551ad905fcedc6f55185ca45a5bec6f0fa59
julianastahelin <julianastahelin@gmail.com>
2025-03-26 22:20:11 -0300

    feat: add link and line break tags to markdown component

--- components/styledMarkdown.tsx
+++ components/styledMarkdown.tsx
@@ -13,7 +13,12 @@ export function StyledMarkdown({
             className={className}
             components={{
                 h1: ({ node, ...props }) => <h1 className='text-xl font-bold' {...props} />,
-                h2: ({ node, ...props }) => <h2 className='text-lg font-semibold' {...props} />
+                h2: ({ node, ...props }) => <h2 className='text-lg font-semibold' {...props} />,
+                a(props) {
+                    const { node, ...rest } = props
+                    return <a {...rest} target='_blank' className='underline' />
+                },
+                br: ({node, ...props}) => <br {...props} />
             }}
         >
             {markdown}
